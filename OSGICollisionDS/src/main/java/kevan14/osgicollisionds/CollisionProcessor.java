752f767377fb983eb721cd5f71e3f0f545b1dd3e
kevan14 <genger-man@hotmail.com>
2017-04-13 20:10:52 +0200

    updated collision to actually detect collision

--- OSGICollisionDS/src/main/java/kevan14/osgicollisionds/CollisionProcessor.java
+++ OSGICollisionDS/src/main/java/kevan14/osgicollisionds/CollisionProcessor.java
@@ -5,6 +5,7 @@
  */
 package kevan14.osgicollisionds;
 
+import kevan14.asteroidscommon.data.Entity;
 import kevan14.asteroidscommon.data.GameData;
 import kevan14.asteroidscommon.data.World;
 import kevan14.asteroidscommon.spi.IEntityProcessingService;
@@ -16,13 +17,36 @@ import kevan14.asteroidscommon.spi.IEntityProcessingService;
  */
 public class CollisionProcessor implements IEntityProcessingService {
 
+    Entity source;
+    
+    
+    /**
+     * Just a simple collision detection using circles overlapping
+     * @param gameData
+     * @param world 
+     */
     @Override
     public void process(GameData gameData, World world) {
          System.out.println("Collision detection...");
+         for(Entity ent : world.getEntities()) {
+             source = ent;
+             
+             for(Entity other : world.getEntities()) {
+                 if(!source.equals(other)) {
+                     
+                     float xDif = source.getX() - other.getX();
+                     float yDif = source.getY() - other.getY();
+                     float distanceSquared = (xDif * xDif) + (yDif * yDif);
+                     
+                     if(distanceSquared < (source.getRadius() + other.getRadius()) * (source.getRadius() + other.getRadius())) {
+                         System.out.println("Collision between: " + source.getType() + " and " + other.getType());
+                     }
+                 }
+             }
+         }
 
     }
 
    
-  
     
 }
