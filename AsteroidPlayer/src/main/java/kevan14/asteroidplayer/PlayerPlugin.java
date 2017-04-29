5541cfd47e806f35f171b4ce737a6c8202a3c77e
kevan14 <genger-man@hotmail.com>
2017-04-29 21:10:25 +0200

    Final commit for delivery Assignment part 2

--- AsteroidPlayer/src/main/java/kevan14/asteroidplayer/PlayerPlugin.java
+++ AsteroidPlayer/src/main/java/kevan14/asteroidplayer/PlayerPlugin.java
@@ -5,6 +5,7 @@
  */
 package kevan14.asteroidplayer;
 
+import java.io.FileNotFoundException;
 import kevan14.asteroidscommon.data.Entity;
 import kevan14.asteroidscommon.data.EntityColor;
 import kevan14.asteroidscommon.data.EntityType;
@@ -25,43 +26,56 @@ import org.springframework.context.support.FileSystemXmlApplicationContext;
 public class PlayerPlugin implements IGamePluginService {
 
     private static ApplicationContext ctx;
-      
-    
-    private Entity player; 
+
+    private Entity player;
 
     @Override
     public void start(GameData gameData, World world) {
+        //Netbeans TestSuite cannot load beans correctly.
+        try{
         ctx = new FileSystemXmlApplicationContext("/asteroidsmodules/modules/lib/beans.xml");
+        }catch (Exception e) {
+            if(e instanceof FileNotFoundException) {
+                ctx = new FileSystemXmlApplicationContext("beans.xml");
+            }
+        }
         
-        player = (Entity) ctx.getBean("playerShip");
+        //not trying to access spring context if it is null.
+        if(ctx != null) {
+           player = (Entity) ctx.getBean("playerShip"); 
+        }
+        
+        
+        //Just adding player instance in case of failure in NBtestSuite
+        if(player == null) {
+            player = createPlayerShip(gameData);
+        }
         System.out.println("Player instance created:" + player);
-        world.addEntity( player);
+        world.addEntity(player);
 
     }
 
-//    private Entity createPlayerShip(GameData gameData) {
-//        Entity playerShip = new Entity();
-//        playerShip.setType(EntityType.PLAYER);
-//        playerShip.setPosition(gameData.getDisplayWidth() / 2, gameData.getDisplayHeight() / 2);
-//        playerShip.setMaxSpeed(100);
-//        playerShip.setAcceleration(100);
-//        playerShip.setDeacceleration(10);
-//        playerShip.setColor(EntityColor.BLUE);
-//        
-//        playerShip.setDx(0);
-//        playerShip.setDy(0);
-//        
-//        playerShip.setRadians(3.1415f / 2);
-//        playerShip.setRadius(1.5f);
-//        playerShip.setRotationSpeed(3);
-//        return playerShip;
-//    }
-
+    private Entity createPlayerShip(GameData gameData) {
+        Entity playerShip = new Entity();
+        playerShip.setType(EntityType.PLAYER);
+        playerShip.setPosition(gameData.getDisplayWidth() / 2, gameData.getDisplayHeight() / 2);
+        playerShip.setMaxSpeed(100);
+        playerShip.setAcceleration(100);
+        playerShip.setDeacceleration(10);
+        playerShip.setColor(EntityColor.BLUE);
+        
+        playerShip.setDx(0);
+        playerShip.setDy(0);
+        
+        playerShip.setRadians(3.1415f / 2);
+        playerShip.setRadius(1.5f);
+        playerShip.setRotationSpeed(3);
+        return playerShip;
+    }
     @Override
     public void stop(GameData gameData, World world) {
         world.removeEntity((Entity) player);
-        ((ConfigurableApplicationContext)ctx).close();
+        ((ConfigurableApplicationContext) ctx).close();
     }
 
 }
-
--- application/src/test/java/kevan14/asteroidsmodules/ApplicationTest.java
+++ application/src/test/java/kevan14/asteroidsmodules/ApplicationTest.java
@@ -6,6 +6,7 @@
 package kevan14.asteroidsmodules;
 
 import java.io.IOException;
+import java.net.URL;
 import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
@@ -21,6 +22,8 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import org.netbeans.junit.NbModuleSuite;
 import org.netbeans.junit.NbTestCase;
+import org.openide.filesystems.FileObject;
+import org.openide.filesystems.FileUtil;
 import org.openide.util.Lookup;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
@@ -31,10 +34,11 @@ import org.xml.sax.SAXException;
  * @author Kennet_Gamer
  */
 public class ApplicationTest extends NbTestCase {
-
-    private static final String ADD_ENEMY_UPDATES_FILE = "C:\\Users\\Kennet_Gamer\\Desktop\\Skole\\Komponenter\\AsteroidsKomponenter\\application\\src\\test\\resources\\enemy\\updates.xml";
-    private static final String REM_ENEMY_UPDATES_FILE = "C:\\Users\\Kennet_Gamer\\Desktop\\Skole\\Komponenter\\AsteroidsKomponenter\\application\\src\\test\\resources\\remenemy\\updates.xml";
-    private static final String UPDATES_FILE = "C:\\Users\\Kennet_Gamer\\Desktop\\Skole\\Komponenter\\AsteroidsKomponenter\\Update_Center\\netbeans_site\\updates.xml";
+    
+    
+    private static final String ADD_ENEMY_UPDATES_FILE = "C:\\Users\\Kennet_Skole\\Desktop\\skole\\komponenter\\AsteroidsKomponenter\\application\\target\\test-classes\\enemy\\enemy_updates.xml";
+    private static final String REM_ENEMY_UPDATES_FILE = "C:\\Users\\Kennet_Skole\\Desktop\\skole\\komponenter\\AsteroidsKomponenter\\application\\target\\test-classes\\remenemy\\noenemy_updates.xml";
+    private static final String UPDATES_FILE = "C:\\Users\\Kennet_Skole\\Desktop\\skole\\komponenter\\AsteroidsKomponenter\\Update_Center\\netbeans_site\\updates.xml";
 
     public ApplicationTest(String name) {
         super(name);
@@ -53,23 +57,23 @@ public class ApplicationTest extends NbTestCase {
         List<IEntityProcessingService> processors = new CopyOnWriteArrayList<>();
         List<IGamePluginService> plugins = new CopyOnWriteArrayList<>();
         
-       // copy(get(REM_ENEMY_UPDATES_FILE), get(UPDATES_FILE), REPLACE_EXISTING);
-       // waitForUpdate(processors, plugins);
+        copy(get(REM_ENEMY_UPDATES_FILE), get(UPDATES_FILE), REPLACE_EXISTING);
+        waitForUpdate(processors, plugins);
 
-       // Assert.assertEquals("Plugins found", 2, plugins.size());
-       // Assert.assertEquals("Processors found", 5, processors.size());
+        Assert.assertEquals("Plugins found", 2, plugins.size());
+        Assert.assertEquals("Processors found", 5, processors.size());
 
-       // copy(get(ADD_ENEMY_UPDATES_FILE), get(UPDATES_FILE), REPLACE_EXISTING);
-       // waitForUpdate(processors, plugins);
+        copy(get(ADD_ENEMY_UPDATES_FILE), get(UPDATES_FILE), REPLACE_EXISTING);
+        waitForUpdate(processors, plugins);
         
-       // Assert.assertEquals("Plugins found", 3, plugins.size());
-       // Assert.assertEquals("Processors found", 6, processors.size());
+        Assert.assertEquals("Plugins found", 3, plugins.size());
+        Assert.assertEquals("Processors found", 6, processors.size());
 
        Assert.assertTrue(true);
     }
 
     private void waitForUpdate(List<IEntityProcessingService> processors, List<IGamePluginService> plugins) throws InterruptedException {
-        Thread.sleep(10000);
+        Thread.sleep(20000);
 
         processors.clear();
         processors.addAll(Lookup.getDefault().lookupAll(IEntityProcessingService.class));
